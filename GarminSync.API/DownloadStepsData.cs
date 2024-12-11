f150eadd1f90d9309f7c1a24bdb1d12aeabe8d53
Anton Mitsengendler <anton_mits@mail.ru>
2024-12-11 22:53:31 +0100

    steps are saved to Azure Tables

--- GarminSync.API/DownloadStepsData.cs
+++ GarminSync.API/DownloadStepsData.cs
@@ -1,4 +1,9 @@
-﻿using GarminSync.API.Settings;
+﻿using Azure;
+using Azure.Data.Tables;
+using Garmin.Connect;
+using Garmin.Connect.Auth;
+using GarminSync.API.Models;
+using GarminSync.API.Settings;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -11,15 +16,35 @@ public class DownloadStepsData(
     IOptions<GarminConnectSettings> garminConnectSettings)
 {
     [Function("DownloadStepsData")]
-    public void Run([TimerTrigger("0 0 */1 * * *", RunOnStartup = true)] TimerInfo myTimer)
+    public async Task Run([TimerTrigger("0 0 */1 * * *", RunOnStartup = true)] TimerInfo myTimer)
     {
-        logger.LogInformation($"Using Garmin.Connect user: {garminConnectSettings.Value.Username}");
-        logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        var userSettings = garminConnectSettings.Value;
+        var tableSettings = azureSettings.Value;
+        
+        logger.LogInformation($"Using Garmin.Connect user: {userSettings.Username}");
+        
+        var authParameters = new BasicAuthParameters(userSettings.Username, userSettings.Password);
+        var client = new GarminConnectClient(new GarminConnectContext(new HttpClient(), authParameters));
+        var stepsData = await client.GetWellnessStepsData(DateTime.Today);
+        
+        logger.LogInformation($"Downloaded {stepsData.Length} wellness data chunks");
+        
+        var tableClient = new TableClient(
+            new Uri(tableSettings.TablesUrl),
+            tableSettings.TableName,
+            new TableSharedKeyCredential(tableSettings.StorageAccountName, tableSettings.StorageAccountSharedKey));
 
-        if (myTimer.ScheduleStatus is not null)
+        foreach (var stepsDataChunk in stepsData.Where(sd => sd.Steps > 0))
         {
-            logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
-            
+            var startUtc = stepsDataChunk.StartGmt.ToUniversalTime();
+            await tableClient.UpsertEntityAsync(new StepsInfoEntity
+            {
+                PartitionKey = startUtc.ToString("yyyy-MM-dd"),
+                RowKey = startUtc.ToString("yyyy-MM-dd HH:mm:ss"),
+                Timestamp = DateTimeOffset.Now,
+                ETag = new ETag(Guid.CreateVersion7().ToString()),
+                Steps = stepsDataChunk.Steps,
+            });
         }
     }
 }
---
+++ GarminSync.API/Models/StepsInfoEntity.cs
@@ -0,0 +1,13 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace GarminSync.API.Models;
+
+public record struct StepsInfoEntity : ITableEntity
+{
+    public string PartitionKey { get; set; }
+    public string RowKey { get; set; }
+    public DateTimeOffset? Timestamp { get; set; }
+    public ETag ETag { get; set; }
+    public long Steps { get; set; }
+}
